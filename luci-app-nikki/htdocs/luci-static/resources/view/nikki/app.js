ab091fe3dd5278cd68411105656d6ee395150190
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-05-12 16:56:55 +0800

    Update app.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
@@ -6,13 +6,13 @@
 'require tools.nikki as nikki';
 
 function renderStatus(running) {
-    return updateStatus(E('input', { id: 'core_status', style: 'border: unset; font-style: italic; font-weight: bold;', readonly: '' }), running);
+    return updateStatus(E('div', { id: 'core_status', style: 'border: unset; font-style: italic; font-weight: bold;', readonly: '' }), running);
 }
 
 function updateStatus(element, running) {
     if (element) {
         element.style.color = running ? 'green' : 'red';
-        element.value = running ? _('Running') : _('Not Running');
+        element.textContent = running ? _('Running') : _('Not Running');
     }
     return element;
 }
@@ -26,6 +26,7 @@ return view.extend({
             nikki.listProfiles()
         ]);
     },
+
     render: function (data) {
         const subscriptions = uci.sections('nikki', 'subscription');
         const appVersion = (data[1] && data[1].app) ? data[1].app : '';
@@ -40,24 +41,21 @@ return view.extend({
         s = m.section(form.TableSection, 'status', _('📊 Status'));
         s.anonymous = true;
 
-        o = s.option(form.Value, '_app_version', _('App Version'));
+        // App Version
+        o = s.option(form.DummyValue, '_app_version', _('App Version'));
         o.readonly = true;
-        o.load = function () {
-            return appVersion;
-        };
-        o.write = function () { };
+        o.load = () => appVersion;
+        o.write = () => {};
 
-        o = s.option(form.Value, '_core_version', _('Core Version'));
+        // Core Version
+        o = s.option(form.DummyValue, '_core_version', _('Core Version'));
         o.readonly = true;
-        o.load = function () {
-            return coreVersion;
-        };
-        o.write = function () { };
+        o.load = () => coreVersion;
+        o.write = () => {};
 
+        // Core Status
         o = s.option(form.DummyValue, '_core_status', _('Core Status'));
-        o.cfgvalue = function () {
-            return renderStatus(running);
-        };
+        o.cfgvalue = () => renderStatus(running);
         poll.add(function () {
             return L.resolveDefault(nikki.status()).then(function (running) {
                 updateStatus(document.getElementById('core_status'), running);
@@ -67,29 +65,21 @@ return view.extend({
         o = s.option(form.Button, 'reload');
         o.inputstyle = 'action';
         o.inputtitle = _('Reload Service');
-        o.onclick = function () {
-            return nikki.reload();
-        };
+        o.onclick = () => nikki.reload();
 
         o = s.option(form.Button, 'restart');
         o.inputstyle = 'negative';
         o.inputtitle = _('Restart Service');
-        o.onclick = function () {
-            return nikki.restart();
-        };
+        o.onclick = () => nikki.restart();
 
         o = s.option(form.Button, 'update_dashboard');
         o.inputstyle = 'positive';
         o.inputtitle = _('Update Dashboard');
-        o.onclick = function () {
-            return nikki.updateDashboard();
-        };
+        o.onclick = () => nikki.updateDashboard();
 
         o = s.option(form.Button, 'open_dashboard');
         o.inputtitle = _('Open Dashboard');
-        o.onclick = function () {
-            return nikki.openDashboard();
-        };
+        o.onclick = () => nikki.openDashboard();
 
         s = m.section(form.NamedSection, 'config', 'config', _('⚙️ App Config'));
 
@@ -101,11 +91,11 @@ return view.extend({
 
         for (const profile of profiles) {
             o.value('file:' + profile.name, _('File:') + profile.name);
-        };
+        }
 
         for (const subscription of subscriptions) {
             o.value('subscription:' + subscription['.name'], _('Subscription:') + subscription.name);
-        };
+        }
 
         o = s.option(form.Value, 'start_delay', _('Start Delay'));
         o.datatype = 'uinteger';
